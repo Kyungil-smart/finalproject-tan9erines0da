6abc57eb9feb3697761f83948f27d7abe70cceee
하용신 <maple77582@gmail.com>
2026-05-28 14:47:46 +0900

    Revert "Merge branch 'Develop' into Comment_SHY"
    This reverts commit cec4a04006b38f29d1de1216cb86791bd8130c4a, reversing
    changes made to a29a7e7f274eda87c27e33b16df3d32c7179ac85.

--- MeowMeow/Assets/Utility/GoogleSheetParse/03.Manager/Test2.cs
+++ MeowMeow/Assets/Utility/GoogleSheetParse/03.Manager/Test2.cs
@@ -1,5 +1,4 @@
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Test2 : MonoBehaviour
 {
@@ -10,16 +9,13 @@ public class Test2 : MonoBehaviour
     public void dsds()
     {
         // 1. 매니저에서 Char2Data(SO)를 가져옵니다. 반환 타입이 Char2Data로 깔끔하게 나옵니다!
-        var charSO = Manager.GetClassData<comment>();
+        var charSO = Manager.GetClassData<PlayerData>();
 
         if (charSO != null)
         {
-            var Listdata = charSO.m_Data;
-            var saveList= Listdata.Where(x=>x.type == CommentType.Butler_Life).ToList();
-            foreach (var item in saveList)
-            {
-                Debug.Log($"ID: {item.word} :{item.type} \n");
-            }
+            // 2. 형변환할 필요 없이 자식 클래스의 메서드를 바로 쓸 수 있습니다.
+            var myAttackInfo = charSO.FindById("1");
+            Debug.Log($"공격력: {myAttackInfo.m_Attack}");
         }
     }
 }
--- MeowMeow/Assets/Utility/GoogleSheetParse/05.So_Class/daesgeulSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(menuName = "SO/daesgeulSO", fileName = "daesgeulSO_")]
-public class daesgeulSO : SheetDataSO<daesgeul>
-{
-
-}
-
