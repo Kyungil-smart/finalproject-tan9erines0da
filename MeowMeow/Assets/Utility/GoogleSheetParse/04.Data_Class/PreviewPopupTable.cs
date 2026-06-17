e68e73f37d35f959c49b51861f590039d5bc37ed
Geunhyung Song <248142774+gamyeonx@users.noreply.github.com>
2026-06-17 15:04:52 +0900

    [feat] 미리보기 팝업 구글 시트 데이터 파싱 및 미리보기 스크립트 팝업 수정

---
+++ MeowMeow/Assets/Utility/GoogleSheetParse/04.Data_Class/PreviewPopupTable.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PreviewPopupTable : Basedata
+{
+    [SerializeField] private string m_Name;
+    [SerializeField] private string m_Resource;
+    [SerializeField] private string m_Description;
+
+    public string Name => m_Name;
+    public string Resource => m_Resource;
+    public string Description => m_Description;
+
+    public override void ApplyRowData(string[] Data)
+    {
+        this.uniqueId = Data[0];
+        this.m_Name = Data[1];
+        this.m_Resource = Data[2];
+        this.m_Description = Data[3];
+    }
+}
