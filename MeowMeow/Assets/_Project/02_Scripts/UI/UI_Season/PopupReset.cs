324b49ee025faf170dd9cef839a270327c90d46d
Geunhyung Song <248142774+gamyeonx@users.noreply.github.com>
2026-06-23 09:54:53 +0900

    [feat] 리셋버튼 코드 수정

--- MeowMeow/Assets/_Project/02_Scripts/UI/UI_Season/PopupReset.cs
+++ MeowMeow/Assets/_Project/02_Scripts/UI/UI_Season/PopupReset.cs
@@ -53,15 +53,16 @@ public class PopupReset : MonoBehaviour, IPopupable
         _contentPresenter.ClosePopup(this);
     }
 
-    void OnResetButtonClick()
+   async void OnResetButtonClick()
     {
         if (_contentPresenter == null) return;
         // 중복 클릭 방지
         _resetButton.interactable = false;
 
-        _contentPresenter.ClosePopup(this);
         // 보드 초기화 실행
-        _contentPresenter.ResetGachaBlocks();
+       await _contentPresenter.ResetGachaBlocks();
+
+        _contentPresenter.ClosePopup(this);
     }
 
     void OnDestroy()
