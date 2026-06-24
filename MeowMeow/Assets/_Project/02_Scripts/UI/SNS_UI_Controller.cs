e860f49d387c53b9457608c3953f7a672123a002
강세환(플밍4기) <ghks5658@gmail.com>
2026-06-24 14:31:59 +0900

    [fix]스티커 삭제버튼 레이어, 누적 보상 팝업 문제 수정

--- MeowMeow/Assets/_Project/02_Scripts/UI/SNS_UI_Controller.cs
+++ MeowMeow/Assets/_Project/02_Scripts/UI/SNS_UI_Controller.cs
@@ -238,7 +238,10 @@ public class SNS_UI_Controller : BaseScreenController
             _lastActiveDepth1Panel != null)
         {
             var clearable = CurrentActivePanel.GetComponent<ISNSPanelClearable>();
-            clearable.ClearPanelContext();
+            if (clearable != null)
+            {
+                clearable.ClearPanelContext();
+            }
 
             // 2깊이 장막을 걷어내는 무조건적인 클로즈 연출 실행
             CurrentActivePanel.Close(() =>
